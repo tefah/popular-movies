a548a3e5753e9c58054bd48fab838a85a6dba0f8
TEFA <mustafa.adel072@gmail.com>
2017-02-20 23:19:56 +0200

    garbage collected

--- app/src/main/java/com/tefah/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/tefah/popularmovies/DetailsActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -14,7 +13,7 @@ import com.squareup.picasso.Picasso;
 public class DetailsActivity extends AppCompatActivity {
 
     Movie movie;
-    ImageView poster, backdrop, share, back;
+    ImageView poster, backdrop;
     TextView  name, releaseDate, overview, rate, title;
     RatingBar ratingBar;
     @Override
@@ -28,12 +27,12 @@ public class DetailsActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(movie.getTitle().trim());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         poster      = (ImageView) findViewById(R.id.poster_view);
         backdrop    = (ImageView) findViewById(R.id.backdrop);
-        share       = (ImageView) findViewById(R.id.share);
-        back        = (ImageView) findViewById(R.id.back);
         name        = (TextView)  findViewById(R.id.name);
         releaseDate = (TextView)  findViewById(R.id.date);
         overview    = (TextView)  findViewById(R.id.overview);
@@ -44,25 +43,13 @@ public class DetailsActivity extends AppCompatActivity {
 
 
         bind();
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
 
     }
     public void bind(){
         Picasso.with(this).load(QueryUtils.creatImageUrlSmall(movie.getPosterPath())).into(poster);
         Picasso.with(this).load(QueryUtils.creatImageUrl(movie.getBackdropPath())).into(backdrop);
         name.setText(movie.getTitle());
-        title.setText(movie.getTitle().trim());
         releaseDate.setText(movie.getReleaseDate());
         overview.setText(movie.getOverview());
         ratingBar.setMax(10);
--- app/src/main/java/com/tefah/popularmovies/MainActivity.java
+++ app/src/main/java/com/tefah/popularmovies/MainActivity.java
@@ -5,18 +5,13 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
